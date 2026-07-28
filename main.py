7d9a4a60460ba0e8a4169b957dec3593abf9cefa
SUDHANSHU NANDAN <sdntheone@gmail.com>
2026-07-28 15:25:02 +0530

    feat(api): wire api_v2 routers into main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from app.api_v2.upload_routes import router as upload_router
+from app.api_v2.document_routes import router as document_router
+from app.api_v2.chat_stream_routes import router as chat_stream_router
+from app.api_v2.observability_routes import router as observability_router
 
 app = FastAPI(
     title="RAGnosis API",
@@ -7,3 +11,7 @@ app = FastAPI(
 )
 
 app.include_router(router)
+app.include_router(upload_router)
+app.include_router(document_router)
+app.include_router(chat_stream_router)
+app.include_router(observability_router)
